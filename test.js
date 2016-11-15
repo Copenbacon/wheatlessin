12d56d2dcd28674409a34ac7be65ad7459243bfd
Joey DeRosa <joeyderosa11.jd@gmail.com>
2016-11-15 11:40:28 -0800

    hi

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
 'use strict';
-$('#results').css({'background-color': 'rgba(0,0,0,0)', 'height': '400px', 'overflow': 'scroll', 'align-items': 'center', 'margin-top': '0px'});
-$('#search-section').css({'margin-bottom': '0px', 'margin-top': '0px', 'transform': 'scale(.5)'});
+// if(businessesArray.length) {
+  $('#results').css({'background-color': 'rgba(0,0,0,0)', 'height': '400px', 'overflow': 'scroll'});
+  $('#infoDisplay').css({'align-items': 'center', 'margin-top': '100px'});
+  $('#search-section').css({'margin-bottom': '0px', 'margin-top': '0px', 'transform': 'scale(.5)'});
+// }
