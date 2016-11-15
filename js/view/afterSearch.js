5f4280c575fe3842823d0ab9f4017e7f616cfa0a
Joey DeRosa <joeyderosa11.jd@gmail.com>
2016-11-15 13:14:44 -0800

    shrink searchbar when viewing list

---
+++ js/view/afterSearch.js
@@ -0,0 +1,6 @@
+'use strict';
+function afterSearch() {
+  $('#results').css({'background-color': 'rgba(0,0,0,0)', 'height': '400px', 'overflow-y': 'scroll', 'overflow-x': 'hidden'});
+  $('#infoDisplay').css({'align-items': 'center', 'margin-top': '100px'});
+  $('#search-section').css({'margin-bottom': '0px', 'margin-top': '0px', 'transform': 'scale(.7)'});
+}
--- test.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-// if(businessesArray.length) {
-  $('#results').css({'background-color': 'rgba(0,0,0,0)', 'height': '400px', 'overflow': 'scroll'});
-  $('#infoDisplay').css({'align-items': 'center', 'margin-top': '100px'});
-  $('#search-section').css({'margin-bottom': '0px', 'margin-top': '0px', 'transform': 'scale(.5)'});
-// }
