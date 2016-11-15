33d6f8360e736b3812311754881a23ec0dc70295
Joey DeRosa <joeyderosa11.jd@gmail.com>
2016-11-15 11:28:04 -0800

    test list scroll

---
+++ test.js
@@ -0,0 +1,3 @@
+'use strict';
+$('#results').css({'background-color': 'rgba(0,0,0,0)', 'height': '400px', 'overflow': 'scroll', 'align-items': 'center', 'margin-top': '0px'});
+$('#search-section').css({'margin-bottom': '0px', 'margin-top': '0px', 'transform': 'scale(.5)'});
